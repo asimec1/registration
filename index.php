ec64e557ade66cc0e5002f8539015d97ae3d4ced
Alen Šimec <alen@eburza.hr>
2019-10-31 17:43:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php 
+	
+	
+	# Start session
+    session_start();
+	
+	# Connect to MySQL database
+	$MySQL = mysqli_connect("localhost","root","","webprog") or die('Error connecting to MySQL server.');
+	
+	# Variables MUST BE INTEGERS
+    if(isset($_GET['menu'])) { $menu   = (int)$_GET['menu']; }
+	if(isset($_GET['action'])) { $action   = (int)$_GET['action']; }
+	
+	# Variables MUST BE STRINGS A-Z
+    if(!isset($_POST['_action_']))  { $_POST['_action_'] = FALSE;  }
+	
+	if (!isset($menu)) { $menu = 1; }
+	
+	
+print '
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="style.css">
+		<!-- End CSS -->
+		<!-- meta elements -->
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="description" content="some description">
+        <meta name="keywords" content="keyword 1, keyword 2, keyword 3, keyword 4, ...">
+		
+		
+        <meta name="author" content="alen@tvz.hr">
+		<!-- favicon meta -->
+		<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+		<!-- end favicon meta -->
+		<!-- end meta elements -->
+		
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"> 
+		<!-- End Google Fonts -->
+		<title>Example page - HTML5</title>
+	</head>
+<body>
+	
+	<main>';
+		if (isset($_SESSION['message'])) {
+			print $_SESSION['message'];
+			unset($_SESSION['message']);
+		}
+	
+		print '
+	<h1>Registration Form</h1>
+	<div id="register">';
+	
+	if ($_POST['_action_'] == FALSE) {
+		print '
+		<form action="" id="registration_form" name="registration_form" method="POST">
+			<input type="hidden" id="_action_" name="_action_" value="TRUE">
+			
+			<label for="fname">First Name *</label>
+			<input type="text" id="fname" name="firstname" placeholder="Your name.." required>
+
+			<label for="lname">Last Name *</label>
+			<input type="text" id="lname" name="lastname" placeholder="Your last natme.." required>
+				
+			<label for="email">Your E-mail *</label>
+			<input type="email" id="email" name="email" placeholder="Your e-mail.." required>
+			
+			<label for="username">Username:* <small>(Username must have min 5 and max 10 char)</small></label>
+			<input type="text" id="username" name="username" pattern=".{5,10}" placeholder="Username.." required><br>
+			
+									
+			<label for="password">Password:* <small>(Password must have min 4 char)</small></label>
+			<input type="password" id="password" name="password" placeholder="Password.." pattern=".{4,}" required>
+
+			<label for="country">Country:</label>
+			<select name="country" id="country">
+				<option value="">molimo odaberite</option>';
+				#Select all countries from database webprog, table countries
+				$query  = "SELECT * FROM countries";
+				$result = @mysqli_query($MySQL, $query);
+				while($row = @mysqli_fetch_array($result)) {
+					print '<option value="' . $row['country_code'] . '">' . $row['country_name'] . '</option>';
+				}
+			print '
+			</select>
+
+			<input type="submit" value="Submit">
+		</form>';
+	}
+	else if ($_POST['_action_'] == TRUE) {
+		
+		$query  = "SELECT * FROM users";
+		$query .= " WHERE email='" .  $_POST['email'] . "'";
+		$query .= " OR username='" .  $_POST['username'] . "'";
+		$result = @mysqli_query($MySQL, $query);
+		$row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
+		
+		if ($row['email'] == '' || $row['username'] == '') {
+			# password_hash https://secure.php.net/manual/en/function.password-hash.php
+			# password_hash() creates a new password hash using a strong one-way hashing algorithm
+			$pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT, ['cost' => 12]);
+			
+			$query  = "INSERT INTO users (firstname, lastname, email, username, password, country)";
+			$query .= " VALUES ('" . $_POST['firstname'] . "', '" . $_POST['lastname'] . "', '" . $_POST['email'] . "', '" . $_POST['username'] . "', '" . $pass_hash . "', '" . $_POST['country'] . "')";
+			$result = @mysqli_query($MySQL, $query);
+			
+			# ucfirst() — Make a string's first character uppercase
+			# strtolower() - Make a string lowercase
+			echo '<p>' . ucfirst(strtolower($_POST['firstname'])) . ' ' .  ucfirst(strtolower($_POST['lastname'])) . ', thank you for registration </p>
+			<hr>';
+		}
+		else {
+			echo '<p>User with this email or username already exist!</p>';
+		}
+	}
+	print '
+	</div>';
+	
+	print '
+	</main>
+	
+</body>
+</html>';
+?>
